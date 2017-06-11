ea6b527012c6d2c83fc56dc9b4042d3b0d6610b1
Sergey Litvinov <slitvinov@gmail.com>
2017-06-12 00:09:29 +0200

    Add -t option to ssh

--- r.cmd
+++ r.cmd
@@ -9,4 +9,6 @@ h=$1; shift
 d=$1; shift
 
 ask_git
-ssh "$h" cd \""$d"\"   '&&'   cd \""$p"\"   '&&'   "$@"
+
+# -t to kill after disconnect
+ssh -t "$h" cd \""$d"\"   '&&'   cd \""$p"\"   '&&'   "$@"
